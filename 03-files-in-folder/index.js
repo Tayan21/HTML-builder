4fd26c82ad5a0592a3488f514a6dd9d9c1034508
Tayan <bostybaev@list.ru>
2023-05-08 13:20:30 +0600

    feat: add code to task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdir(path.join(__dirname, 'secret-folder'),
+  { withFileTypes: true },
+  (err, files) => {
+  console.log("\nCurrent directory files:");
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+
+      if (!file.isDirectory()) {
+        let fileName = (file.name).split('.')
+        fs.stat(path.join(__dirname, 'secret-folder', file.name), (error, stats) => {
+          if (error) {
+            console.log(error);
+          }
+          console.log(`${(fileName[0])} - ${path.extname(file.name).slice(1)} - ${stats.size} bytes`);
+        });
+        
+      }
+      
+    })
+  }
+})

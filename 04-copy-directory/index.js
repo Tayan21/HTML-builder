3629eb2903e7430caf846c742caabe4e7fc4d9f4
Tayan <bostybaev@list.ru>
2023-05-09 00:03:19 +0600

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,34 +2,33 @@ const fs = require('fs');
 const path = require('path');
 const fsPromises = fs.promises;
 
-fsPromises
-  .mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
-  .then(function () {
-    console.log('Directory created successfully');
-  })
-  .catch(function () {
-    console.log('failed to create directory');
-  });
+(async function () {
 
-fs.readdir(
-  path.join(__dirname, 'files'),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) console.log(err);
-    else {
-      files.forEach((file) => {
-        fs.copyFile(
-          path.join(__dirname, 'files', file.name),
-          path.join(__dirname, 'files-copy', file.name),
-          (err) => {
-            if (err) {
-              console.log('Error Found:', err);
-            } else {
-              console.log('file copied');
-            }
-          }
-        );
-      });
-    }
+  await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+  const copyFiles = await fsPromises.readdir(path.join(__dirname, 'files-copy'));
+  for (const file of copyFiles) {
+    await fsPromises.rm(path.join(__dirname, 'files-copy', file), { force: true });
   }
-);
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    { withFileTypes: true },
+    (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          fs.copyFile(
+            path.join(__dirname, 'files', file.name),
+            path.join(__dirname, 'files-copy', file.name),
+            (err) => {
+              if (err) {
+                console.log('Error Found:', err);
+              } else {
+                console.log('file copied');
+              }
+            }
+          );
+        });
+      }
+    }
+  );
+})();
